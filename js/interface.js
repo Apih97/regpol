07dd60beec53743954c4ad0f1c42f4cf5026f4af
Carlos Peña <corochena@gmail.com>
2018-11-27 19:17:46 -0600

    Regresion polinomial completo con interfaz

--- js/interface.js
+++ js/interface.js
@@ -1,6 +1,6 @@
 var order=2, n=9;
 
-loadSample([[0,0,1,2,0,1,2,2,1],[0,2,2,4,4,6,6,2,1],[14,21,11,12,23,23,14,6,11]]);
+loadSample([[0,1,2,3,4,5],[2.1,7.7,13.6,27.2,40.9,61.1]]);
 
 function crearTabla(rows, cols) {
   var esp = document.querySelector(".datainput");
@@ -10,11 +10,11 @@ function crearTabla(rows, cols) {
 
   for (var i = 0; i <= rows; i++) {
     var fila = document.createElement("tr");
-    for (var j = 0; j <= cols; j++) {
+    for (var j = 0; j <= 2; j++) {
       var celda = document.createElement("td");
       if (i==0) {
         if (j>0)
-          celda.innerHTML = (j==cols ? "Y":"X<sub>" + j +"</sub>");
+          celda.innerHTML = (j==2 ? "Y":"X" );
       }
       else
         celda.innerHTML = (j==0 ? "<sub>" + i + "</sub>": "<input>");
@@ -56,15 +56,15 @@ document.querySelector("button").addEventListener("click", function() {
 
   for (var i = 0; i < elementos.length; i++) {
     var v = +elementos[i].value;
-    if (i <= order) {
+    if (i <= 1) {
       data.push([]);
       data[i].push(v);
     }
     else
-      data[i%(order+1)].push(v);
+      data[i%(2)].push(v);
   }
 
-  var reg = regresion(data);
+  var reg = regresion(data,order);
   
   var results = document.querySelector("#results");
   var txt = "<h1>Resultados</h1>Ecuación: " + equation(reg[0]) + "<br>";
@@ -75,8 +75,8 @@ document.querySelector("button").addEventListener("click", function() {
 
 function equation(coefs) {
   return coefs.reduce(function(str,coef,i) {
-    str += (coef > 0 ? "+ ":"- ") + Math.abs(coef.toFixed(3)) + " ";
-    if (i>0) str += "X<sub>" + i + "</sub> ";
+    str += (coef > 0 ? "+ ":"- ") + Math.abs(coef.toFixed(5)) + " ";
+    if (i>0) str += "X<sup>" + i + "</sup> ";
     return str;
   },"")
 }
